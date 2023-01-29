e0a949c0806528553511c76285328592874094de
mngvn <95495029+mngvn@users.noreply.github.com>
2023-01-29 02:55:25 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function signedUp(){
-    
+let members = []
+const addMember = (ev)=> {
+    ev.preventDefault();
+    let person = {
+        firstName: document.getElementById('fname').value,
+        lastName: document.getElementById('lname').value,
+        email: document.getElementById('email').value,
+        location: document.getElementById('location').value,
+    }
+    members.push(person);
+    document.querySelector('form').reset();
+
 }
+    console.log(members);
+    document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('button').addEventListener('click', addUser);
+
+})
+
+    
